543cc34f213480fd3816b0de24de3e6ecb18fd53
guguoqing <826327516@qq.com>
2016-10-28 06:38:22 +0800

    2016年10月28日06:32:34

--- src/com/hasyou/demo/HelloWorld.java
+++ src/com/hasyou/demo/HelloWorld.java
@@ -9,7 +9,8 @@ public class HelloWorld {
 
         System.out.println("hello world!");
         System.out.println("hello intellij!   我们都是中国人!");
-
+        System.out.println("我还是要征服整个世界咯!");
+        System.out.println("2016年10月28日06:37:22");
     }
 
 }
