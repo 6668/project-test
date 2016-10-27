041543516e02d284de1b37c7d7a37f28027da775
guguoqing <826327516@qq.com>
2016-10-28 06:20:59 +0800

    就是想上传一下试一试咯!

---
+++ src/com/hasyou/demo/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.hasyou.demo;
+
+/**
+ * Created by Going on 2016/10/5.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world!");
+        System.out.println("hello intellij!   我们都是中国人!");
+
+    }
+
+}
